2adfb42daa5187fc17ed3cf2496ea79b601cf253
y2eunishere <68018786+y2eunishere@users.noreply.github.com>
2020-08-01 15:22:28 +0900

    3w_hw_genie
    3주차 파이썬 웹사이트자료모으기

---
+++ 3week_homework_genie.py
@@ -0,0 +1,35 @@
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta
+
+
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# 아래 빈 칸('')을 채워보세요
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+
+#body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
+
+for tr in trs :
+    title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+    rank = tr.select_one('td.number').text[0:2].strip()
+    artist = tr.select_one('td.info >a.artist.ellipsis').text
+
+    doc = {
+        'title' : title,
+        'rank' : rank,
+        'artist' :artist
+    }
+
+    db.genie.insert_one(doc)
